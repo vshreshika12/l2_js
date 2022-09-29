bb0b319c5fecee90ec8f6bde5274fdbb18a5d86d
vshreshika12 <113594749+vshreshika12@users.noreply.github.com>
2022-09-29 00:00:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+let port = process.argv.slice(2)
+port = parseInt(String(port).slice(7))
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent=""
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+        case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port);
